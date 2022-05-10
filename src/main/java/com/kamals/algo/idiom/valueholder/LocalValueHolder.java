0dc219bb7e7b48df9da3e52fd7f8df17f7509c6e
Kamal <kamal.jgd@gmail.com>
2022-05-11 00:07:45 +0530

    Value Holder Reentrant ReadWrite Lock

--- src/main/java/com/kamals/algo/idiom/valueholder/LocalValueHolder.java
+++ src/main/java/com/kamals/algo/idiom/valueholder/LocalValueHolder.java
@@ -1,11 +1,21 @@
 package com.kamals.algo.idiom.valueholder;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+@Component("localValueHolder")
 public class LocalValueHolder implements ValueHolder
 {
-    private static final Map<String, Value> valueMap = new HashMap<>();
+    private static final Map<String, Value> valueMap = new ConcurrentHashMap<>();
+    private static final Map<String, ReentrantReadWriteLock> lockMap = new ConcurrentHashMap<>();
+
+    @Autowired
+    private RemoteValueHolder remoteValueHolder;
 
     @Override
     public Value getValue(String name)
@@ -13,14 +23,23 @@ public class LocalValueHolder implements ValueHolder
         Value value = valueMap.get(name);
         if (value == null)
         {
-            synchronized (this)
+            ReentrantReadWriteLock reentrantReadWriteLock = getLock(name);
+            reentrantReadWriteLock.writeLock().lock();
+            try
             {
                 value = valueMap.get(name);
                 if (value == null)
                 {
-                    value = Value.generateValue();
-                    valueMap.put(name, value);
+                    value = remoteValueHolder.getValue(name);
+                    if (value != null)
+                    {
+                        setValue(name, value);
+                    }
                 }
+            }
+            finally
+            {
+                reentrantReadWriteLock.writeLock().unlock();
             }
         }
         return value;
@@ -36,5 +55,23 @@ public class LocalValueHolder implements ValueHolder
     public void deleteValue(String name)
     {
         valueMap.remove(name);
+    }
+
+    private ReentrantReadWriteLock getLock(String name)
+    {
+        ReentrantReadWriteLock reentrantReadWriteLock = lockMap.get(name);
+        if (reentrantReadWriteLock == null)
+        {
+            synchronized (this) //Is it needed? Can be improved?
+            {
+                reentrantReadWriteLock = lockMap.get(name);
+                if (reentrantReadWriteLock == null)
+                {
+                    reentrantReadWriteLock = new ReentrantReadWriteLock();
+                    lockMap.put(name, reentrantReadWriteLock);
+                }
+            }
+        }
+        return reentrantReadWriteLock;
     }
 }
---
+++ src/main/java/com/kamals/algo/idiom/valueholder/RemoteValueHolder.java
@@ -0,0 +1,52 @@
+package com.kamals.algo.idiom.valueholder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+@Component("remoteValueHolder")
+public class RemoteValueHolder implements ValueHolder
+{
+    private static final Map<String, Value> remoteValueMap = new ConcurrentHashMap<>();
+    private static final Map<String, ReentrantReadWriteLock> lockMap = new HashMap<>();
+
+    @Autowired
+    private LocalValueHolder localValueHolder;
+
+    @Override
+    public Value getValue(String name)
+    {
+        Value value = localValueHolder.getValue(name);
+        if (value == null)
+        {
+            synchronized (this) //Need Global Locking here..(Redis, Hz etc.)
+            {
+                value = localValueHolder.getValue(name);
+                if (value == null)
+                {
+                    value = Value.generateValue();
+                    setValue(name, value);
+                }
+            }
+        }
+        return value;
+    }
+
+    @Override
+    public void setValue(String name, Value value)
+    {
+        remoteValueMap.put(name, value);
+        localValueHolder.setValue(name, value);
+    }
+
+    @Override
+    public void deleteValue(String name)
+    {
+        remoteValueMap.remove(name);
+        localValueHolder.deleteValue(name);
+    }
+}
